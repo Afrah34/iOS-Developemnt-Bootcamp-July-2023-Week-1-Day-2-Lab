0da7eb99320d7e54b2931823db3f4b020aed4060
Afrah34 <140310063+Afrah34@users.noreply.github.com>
2023-07-28 17:54:27 +0300

    Add files via upload
    Afrah

---
+++ Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//task 1
+var score = 5
+score += 10
+print(score)
+
+//task2
+if score>9
+{
+    print("Pass")
+} else
+{
+    print("Fail")
+}
+
+//task3
+var names:[String]=["Ahmed","Ail","Faisal","Saud","Salim"]
+for i in names{
+    print(i)
+}
+
+//tase4
+var fruit:[String:Int] = ["apple": 3 , "orange": 4 ,"banana":5 ]
+for i in fruit{
+    print(i)
+}
+
+//tase5
+let name = "Afrah"
+print("welcome " + name)
